d089ab832ef646aabd7fd350ee92a1d3e39c3c65
ᴅᴇᴠᴀɴsʜ <125127646+Devansh-Bots@users.noreply.github.com>
2023-07-09 10:41:39 +0530

    Update config.py
    Signed-off-by: ᴅᴇᴠᴀɴsʜ <125127646+Devansh-Bots@users.noreply.github.com>

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "5682328579"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/af2b45f1aff30c6c5f4f1.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3001ea3ef300201820f2b.jpg")
@@ -19,8 +19,8 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3001ea3ef300201820f2b.
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NotMySpace")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DadEyeBotz")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1794184828").split()))
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Professorgurubar")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5682328579").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
